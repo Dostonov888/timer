6e222daca889a9f1e785269ca7674bed2a95f6e0
Jamshid Dostonov <Dostonov888@yandex.ru>
2021-02-28 18:04:59 +0300

    ДЗ первого задание

--- script/script.js
+++ script/script.js
@@ -20,12 +20,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
             return { timeRemaining, day, hours, minutes, seconds };
         }
+
         function updateClock() {
             let timer = getTimeRemaining();
 
             timerHours.textContent = ('0' + timer.hours).slice(-2);
             timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
             timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
+
             timerDay.textContent = 'До конца акции осталось ' + timer.day + ' дней';
             if (timer.timeRemaining <= 0) {
                 clearInterval(1);
