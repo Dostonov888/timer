3d7f08d025a71294b1e0a4e3409dbd9c11666666
Жам <Dostonov888@yandex.ru>
2021-03-03 01:21:26 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,45 +1,4 @@
 
 
-window.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-    //timer
-    function countTimer(deadline) {
-        let timerHours = document.querySelector('#timer-hours'),
-            timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds'),
-            timerDay = document.querySelector('.timer-action');
-        console.log(timerDay);
-        function getTimeRemaining() {
-            let dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor(timeRemaining % 60),
-                minutes = Math.floor((timeRemaining / 60) % 60),
-                hours = Math.floor(timeRemaining / 60 / 60),
-                day = Math.floor(timeRemaining / 60 / 60 / 24);
-
-            return { timeRemaining, day, hours, minutes, seconds };
-        }
-
-        function updateClock() {
-            let timer = getTimeRemaining();
-
-            timerHours.textContent = ('0' + timer.hours).slice(-2);
-            timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
-            timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
-
-            timerDay.textContent = 'До конца акции осталось ' + timer.day + ' дней';
-            if (timer.timeRemaining <= 0) {
-                clearInterval(1);
-            }
-
-        }
-        updateClock();
-
-        setInterval(updateClock, 1000);
-    }
-    countTimer('31 december 2021,00:00:00');
-
-});
-
+//папка не правильно создано
 
