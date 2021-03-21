bac47c044004e1db997719e23d2475e74400107d
Jamshid Dostonov <Dostonov888@yandex.ru>
2021-03-21 23:37:43 +0300

    поправки

--- script/script.js
+++ script/script.js
@@ -425,17 +425,26 @@ window.addEventListener('DOMContentLoaded', function () {
             });
         });
         const postData = (formData) => {
-            return fetch('./server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: formData
+
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.addEventListener('readystatechange', () => {
+
+                    if (request.readyState !== 4) {
+                        return;
+                    }
+                    if (request.status === 200) {
+                        resolve();
+                    } else {
+                        reject(request.status);
+                    }
+                });
+                request.open('POST', './server.php');
+                request.setRequestHeader('Content-Type', 'application/json');
+                request.send(JSON.stringify(formData));
             });
         };
+        sendForm();
+
     };
-    sendForm();
-
-
-
 });
